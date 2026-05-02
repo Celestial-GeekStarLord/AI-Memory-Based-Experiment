c0bd68b4e8c5d5e01321173052d726a535f0d247
Nabin Joshi <cubesat007@gmail.com>
2026-05-02 23:09:20 +0545

    exp 3 camera

--- camera1.py
+++ camera1.py
@@ -1,13 +1,12 @@
 import cv2
 
-# Open the default camera
+
 cam = cv2.VideoCapture(0)
 
-# Get the default frame width and height
 frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# Define the codec and create VideoWriter object
+
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
@@ -20,7 +19,6 @@ while True:
     # Display the captured frame
     cv2.imshow('Camera', frame)
 
-    # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
         break
 
